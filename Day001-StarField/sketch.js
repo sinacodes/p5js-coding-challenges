05a24312fff15b0cc60dbfe7e635d4191ff50c4e
Sina Burow <sina.burow@sinnerschrader.com>
2018-12-03 15:37:27 +0100

    start fresh

--- Day001-StarField/sketch.js
+++ Day001-StarField/sketch.js
@@ -1,29 +1,7 @@
-var stars = [];
-var speed;
-
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  for (var i = 0; i < 800; i++) {
-    stars[i] = new Star();
-  }
+
 }
 
 function draw() {
-  // speed = map(mouseX, 0, width, 0, 50);
-  speed = map(Math.abs(pos), -600, 600, 0, 50);
-  if (speed > 50) {
-    speed = 50;
-  }
-  background(0);
 
-  translate(width / 2, height / 2);
-  for (var i = 0; i < stars.length; i++) {
-    stars[i].update();
-    stars[i].show();
-  }
-}
-
-var pos = 0;
-function mouseWheel(event) {
-  pos += event.delta;
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# p5.js playground.
+
+coding challanges from daniel shiffmans youtube channel [the coding train](https://www.youtube.com/user/shiffman).
